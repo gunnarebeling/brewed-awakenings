f355012312d6d232c62388669b13708a4f78a978
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-09 14:15:21 -0500

    added clickEvent to products

--- scripts/Products.js
+++ scripts/Products.js
@@ -6,11 +6,24 @@ export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li data-type="product" data-name="${product.name}" data-price="${product.price.toFixed(2)}">${product.name}</li>`
     }
 
     html += "</ul>"
 
     return html
 }
+
+document.addEventListener("click", (clickEvent)=>{
+    const itemClicked = clickEvent.target;
+    const productName = itemClicked.dataset.name;
+    const productPrice = itemClicked.dataset.price;
+    if (itemClicked.dataset.type === "product") {
+   
+        window.alert(`${productName} costs\n$${productPrice}`)
+        
+    }
+    
+
+})
 
