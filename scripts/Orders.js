8d13f1205343a04266757217650ad3d904fdd7e7
Gunnar Ebeling <gunnarebeling@icloud.com>
2024-08-09 11:58:01 -0500

    fixed employee and product bugs

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -7,7 +7,7 @@ const orders = getOrders()
 
 
 // Function whose responsibility is to find the product for an order
-const findproduct = (order, allProducts) => {
+const findProduct = (order, allProducts) => {
     let orderProduct = null
 
     for (const product of allProducts) {
@@ -20,12 +20,16 @@ const findproduct = (order, allProducts) => {
 }
 
 // Function whose responsibility is to find the employee for an order
-const findemployee = (order, allEmployees) => {
+const findEmployee = (order, allEmployees) => {
     let orderEmployee = null
 
-    for (const employee in allEmployees) {
+    for (const employee of allEmployees) {
         if (employee.id === order.employeeId) {
             orderEmployee = employee
+        }else{
+            orderEmployee = {
+                name: "not an employee anymore"
+            }
         }
     }
 
@@ -34,11 +38,11 @@ const findemployee = (order, allEmployees) => {
 
 export const Orders = () => {
     let html = ""
-    html = "<ul>"
+    html += "<ul>"
 
     for (const order of orders) {
         const employee = findEmployee(order, employees)
-        const product = findProduct(order)
+        const product = findProduct(order, products)
 
         html += `<li>${product.name} was sold by ${employee.name} on ${new Date(order.timestamp).toLocaleDateString()}</li>`
     }
